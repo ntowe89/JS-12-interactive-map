4150b05599bace88331b46badfe29784ddc8c53a
Nathan Towe <nathanwtowe89@gmail.com>
2023-09-26 03:20:56 -0400

    got most of the fetch done

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ async function main(){
     let userCoord = await getCoords()
     var map = L.map('map').setView([userCoord[0], userCoord[1]], 13)
 
-
-    console.log(userCoord[0].toFixed(2))
-    let myLoc = (userCoord[0].toFixed(2)).toString()+','+(userCoord[1].toFixed(2)).toString()
-    console.log(myLoc)
+    let myLat = (userCoord[0].toFixed(2)).toString()
+    let myLong = (userCoord[1].toFixed(2)).toString()
+    console.log(myLat, myLong)
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -14,39 +13,28 @@ async function main(){
     }).addTo(map);
     
     //Get users choice
+    let myChoice = null
     const myButton = document.getElementById("submitButton")
     myButton.addEventListener("click", myFunction => {
-        let myChoice = document.getElementById('bussiness') 
-        placeSearch(myChoice.value, myLoc)
+        myChoice = document.getElementById('bussiness') 
+        console.log(myChoice.value)
     })
 
     //fetching foursquare
     // api key: fsq3LJTyMVuK+jINq6/8vW2zZI466qt0LrR2C0CwtfOOrY4=
     
-    async function placeSearch(place, loc) {
-        try {
-            const searchParams = new URLSearchParams({
-              query: place,
-              ll: loc,
-              open_now: 'true',
-              sort: 'DISTANCE'
-            });
-            const results = await fetch(
-              `https://api.foursquare.com/v3/places/search?${searchParams}`,
-              {
-                method: 'GET',
-                headers: {
-                  Accept: 'application/json',
-                  Authorization: 'fsq3LJTyMVuK+jINq6/8vW2zZI466qt0LrR2C0CwtfOOrY4=',
-                }
-              }
-            );
-            const data = await results.json();
-            return data;
-        } catch (err) {
-            console.error(err);
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3LJTyMVuK+jINq6/8vW2zZI466qt0LrR2C0CwtfOOrY4='
         }
-    }
+      };
+      
+      fetch('https://api.foursquare.com/v3/places/search?query='+myChoice+'&ll='+ myLat +'%2C'+myLong+'&radius=24140&limit=5', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 }
 
 //get current location
